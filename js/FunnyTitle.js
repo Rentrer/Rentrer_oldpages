7048413916ffb99386038696ba35f25a4fac86bf
Rentrer <1097187031@qq.com>
2022-01-19 18:16:44 +0800

    Site updated: 2022-01-19 18:16:42

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,11 +3,11 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/failure.ico");
+        // $('[rel="icon"]').attr('href', "/failure.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     } else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function() {
             document.title = OriginTitle;
